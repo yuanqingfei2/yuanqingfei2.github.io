3ee354ba16d3366ad1b73cf88a77f1d9252e2f87
Qingfei Yuan <yuanqingfei@gmail.com>
2026-02-11 18:57:38 -0500

    Fix GraphQL queries for Gatsby 5: update sort syntax from deprecated fields to new format

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -98,7 +98,7 @@ export const pageQuery = graphql`
       }
     }
     allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC }
+      sort: { frontmatter: { date: DESC } }
       limit: $limit
       skip: $skip
     ) {
