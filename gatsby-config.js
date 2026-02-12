4efc22afe18a019faaca5f56e8bf1fa9c312ed52
Qingfei Yuan <yuanqingfei@gmail.com>
2026-02-11 21:13:03 -0500

    Remove gatsby-remark-embed-gist plugin: fix 404 CSS errors and StaticQuery issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,19 +30,6 @@ module.exports = {
       options: {
         plugins: [
           {
-            resolve: "gatsby-remark-embed-gist",
-            options: {
-              // Optional:
-    
-              // the github handler whose gists are to be accessed
-              username: 'yuanqingfei',
-    
-              // a flag indicating whether the github default gist css should be included or not
-              // default: true
-              includeDefaultCss: false
-            }
-          },
-          {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
